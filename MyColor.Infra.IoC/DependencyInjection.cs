a7748980b8b61f4a07ff7ce3f6a4667aaf36532c
Joao Rodrigo de Oliveira Ziegler <jr.ziegler@gmail.com>
2021-10-22 21:51:28 +0200

    Add DependencyInjection class to handle with Inversion of Control at startup class

---
+++ MyColor.Infra.IoC/DependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyColor.Domain.Interfaces;
+using MyColor.Infra.Data.Context;
+using MyColor.Infra.Data.Repositories;
+
+namespace MyColor.Infra.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseInMemoryDatabase(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IPersonRepository, PersonRepository>();
+
+            return services;
+
+            // TODO:
+            // in file appsettings add:
+            //"ConnectionStrings": {
+            //  "DefaultConnection": "DbInMemory"
+            //}
+            //
+            // register the service in the startup class
+            // services.AddInfrastructure(Configuration);
+        }
+    }
+}
