ee48eea6322bfb1a211d181a3476c05055dd6e9a
João Rodrigo de Oliveira Ziegler <jr.ziegler@gmail.com>
2021-10-23 19:07:49 +0200

    Add DTO class in the Application project

---
+++ MyColor.Application/DTOs/PersonDTO.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyColor.Application.DTOs
+{
+    public class PersonDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
+        [MaxLength (100)]
+        [DisplayName("name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Lastname is required.")]
+        [MaxLength(100)]
+        [DisplayName("lastname")]
+        public string LastName { get;  set; }
+
+        [MaxLength(10)]
+        [DisplayName("zipcode")]
+        public string ZipCode { get;  set; }
+
+        [MaxLength(100)]
+        [DisplayName("city")]
+        public string City { get;  set; }
+
+        [DisplayName("color")]
+        public string Color { get;  set; }
+
+        public PersonDTO()
+        {
+        }
+    }
+}
